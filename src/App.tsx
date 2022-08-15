2435ebb50b41fa7d1dddb787ebe22e4849916adf
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-15 20:24:21 +0200

    Add NewTodo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import NewTodo from "./components/NewTodo";
 import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const toDos = [{ id: "t1", text: "Finish the course" }];
   return (
     <div className="App">
+      <NewTodo />
       <TodoList items={toDos} />
     </div>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface TodoListPRops {
   items: { id: string; text: string }[];
 }
