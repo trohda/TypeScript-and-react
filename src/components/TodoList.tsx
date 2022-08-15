e11cc8ebcdf2baca1df5f5b8399816a3e27f04be
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-15 20:02:02 +0200

    Working with props

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface TodoListPRops {
+  items: { id: string; text: string }[];
+}
+
+const TodoList: React.FC<TodoListPRops> = (props) => {
+  return (
+    <ul>
+      {props.items.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
